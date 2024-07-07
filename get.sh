c6faf9066f09510bd3c9754da2bb8871ebf1947a
Hasan <mr.k779@outlook.com>
2024-07-08 02:21:20 +0600

    Updates

--- get.sh
+++ get.sh
@@ -1,33 +1,33 @@
 #!/bin/bash
 
-# Function to install Ruby on Ubuntu or Debian
-install_ruby_debian() {
+# Function to install packages on Ubuntu or Debian
+install_packages_debian() {
     sudo apt update
-    sudo apt install -y ruby-full
+    sudo apt install -y ruby-full wget
 }
 
-# Function to install Ruby on CentOS, Fedora, or RHEL
-install_ruby_redhat() {
-    sudo yum install -y ruby
+# Function to install packages on CentOS, Fedora, or RHEL
+install_packages_redhat() {
+    sudo yum install -y ruby wget
 }
 
-# Function to install Ruby on Arch Linux
-install_ruby_arch() {
-    sudo pacman -Sy ruby --noconfirm
+# Function to install packages on Arch Linux
+install_packages_arch() {
+    sudo pacman -Sy ruby wget --noconfirm
 }
 
-# Detect the Linux distribution and install Ruby
+# Detect the Linux distribution and install packages
 if [ -f /etc/os-release ]; then
     . /etc/os-release
     case $ID in
         ubuntu|debian)
-            install_ruby_debian
+            install_packages_debian
             ;;
         centos|fedora|rhel)
-            install_ruby_redhat
+            install_packages_redhat
             ;;
         arch)
-            install_ruby_arch
+            install_packages_arch
             ;;
         *)
             echo "Unsupported distribution: $ID"
@@ -39,7 +39,34 @@ else
     exit 1
 fi
 
-# Download and run the Ruby script
-curl -o setup.rb https://raw.githubusercontent.com/RukuLab/bootstrap/main/setup.rb
-ruby setup.rb
-rm setup.rb
+# Get the latest release information
+REPO="RukuLab/bootstrap"
+RELEASE_INFO=$(curl -s https://api.github.com/repos/$REPO/releases/latest)
+
+# Extract the tag name
+TAG_NAME=$(echo $RELEASE_INFO | jq -r '.tag_name')
+FILE_NAME="$TAG_NAME.tar.gz"
+
+# Construct the download URL
+DOWNLOAD_URL="https://github.com/$REPO/archive/refs/tags/$FILE_NAME"
+
+# Download the file using wget
+wget "$DOWNLOAD_URL"
+
+# Unzip the file
+tar -xzf "$FILE_NAME"
+
+# Get the unzipped directory name
+UNZIPPED_DIR="${FILE_NAME%.tar.gz}"
+
+# Change to the unzipped directory
+cd "$UNZIPPED_DIR"
+
+# Run the Ruby script
+ruby main.rb
+
+# Change back to the original directory
+cd ..
+
+# Remove the zipped file and unzipped directory
+rm -rf "$FILE_NAME" "$UNZIPPED_DIR"
