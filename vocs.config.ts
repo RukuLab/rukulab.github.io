a633ec69c504da6b1854327f942ec3cb869ee669
Rafi <mr.k779@outlook.com>
2024-09-15 02:32:29 -0300

    Upgrade vocs

--- vocs.config.ts
+++ vocs.config.ts
@@ -1,15 +1,15 @@
-import { defineConfig } from 'vocs'
+import { defineConfig } from "vocs";
 
 export default defineConfig({
-  title: 'Docs',
+  title: "Ruku",
   sidebar: [
     {
-      text: 'Getting Started',
-      link: '/getting-started',
+      text: "Getting Started",
+      link: "/getting-started",
     },
     {
-      text: 'Example',
-      link: '/example',
+      text: "Example",
+      link: "/example",
     },
   ],
-})
+});
