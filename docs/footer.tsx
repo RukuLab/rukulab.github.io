06e638fab9e1d0f911f8e4c66e09daec2c1e7de9
Rafi <mr.k779@outlook.com>
2024-10-12 01:29:33 -0300

    footer_license

---
+++ docs/footer.tsx
@@ -0,0 +1,8 @@
+export default function Footer() {
+    return (
+        <div className="footer_license">
+            <p>Released under the MIT License.</p>
+            <p>Copyright © 2024-present RukuLabs.</p>
+        </div>
+    );
+}
